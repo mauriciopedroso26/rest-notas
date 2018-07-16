371f9c7108bb21fa06f05e966ba0c440bbdcf8b5
mauricio.pedroso <mauricio.pedroso@ntconsult.com.br>
2018-07-16 15:35:11 -0300

    Resolve merge

--- src/main/java/br/edu/devmedia/config/BDConfig.java
+++ src/main/java/br/edu/devmedia/config/BDConfig.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class BDConfig {
-	public Connection getConnection() throws ClassNotFoundException, SQLException {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 	    Class.forName("com.mysql.jdbc.Driver");
 	    return DriverManager.getConnection("jdbc:mysql://localhost:3306/notes_db", "root", "root");
 	}
